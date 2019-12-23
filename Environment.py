5f2f26a30ab32957c6163df1306b580b9a2034d3
IdosElmo <36295360+IdosElmo@users.noreply.github.com>
2019-12-23 20:26:48 +0200

    Update Environment.py

--- Environment.py
+++ Environment.py
@@ -1,263 +1,351 @@
-import pygame
+# Snake Tutorial Python
+
+import math
+import random
 import numpy as np
 import sys
-from collections import deque
-import itertools
-import math
-import time
-import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
 
 
-class World:
-    width = 100
-    height = 100
-    time = 0
-    score = 0
-    reward = 0
-    state_size = 25
+class cube(object):
+    rows = 20
+    w = 500
+    pos = 0
+
+    def __init__(self, start, dirnx=1, dirny=0, color=(255, 0, 0)):
+        self.pos = start
+        self.dirnx = 1
+        self.dirny = 0
+        self.color = color
+
+    def move(self, dirnx, dirny):
+        self.dirnx = dirnx
+        self.dirny = dirny
+        self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
+
+    def draw(self, surface, eyes=False):
+        dis = self.w // self.rows
+        i = self.pos[0]
+        j = self.pos[1]
+
+        pygame.draw.rect(surface, self.color, (i * dis + 1, j * dis + 1, dis - 2, dis - 2))
+        if eyes:
+            centre = dis // 2
+            radius = 3
+            circleMiddle = (i * dis + centre - radius, j * dis + 8)
+            circleMiddle2 = (i * dis + dis - radius * 2, j * dis + 8)
+            pygame.draw.circle(surface, (0, 0, 0), circleMiddle, radius)
+            pygame.draw.circle(surface, (0, 0, 0), circleMiddle2, radius)
 
 
-class Snake:
-    def __init__(self):
-        snake_x = World.width * np.random.random()
-        snake_y = World.height * np.random.random()
+class snake(object):
+    body = []
+    turns = {}
+    hit_wall = False
+    done = False
 
-        self.head_position = [int(snake_x), int(snake_y)]
-        self.prev_head_position = [int(snake_x), int(snake_y)]
+    def __init__(self, color, pos):
+        self.done = False
+        self.color = color
+        self.head = cube(pos)
+        self.body.append(self.head)
+        self.dirnx = 0
+        self.dirny = 1
+        self.hit_wall = False
 
-        self.hitbox = 10
-
-        self.snake_positions = deque()
-        self.snake_positions.append(self.head_position)
-        self.body_length = len(self.snake_positions)
-
-    def update_snake(self, state, prev_head_pos):
+    def move(self, action):
+        # pygame.event.
         flag = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
 
-        if state == -1:
-            if snake.body_length > 1:
-                world.reward = snake.body_length
-            else:
-                world.reward = -1.0
-            flag = True
+            # keys = pygame.key.get_pressed()
+            #
+            # for key in keys:
+            #     if keys[pygame.K_LEFT]:
+            #         self.dirnx = -1
+            #         self.dirny = 0
+            #         self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+            #         flag = True
+            #
+            #     elif keys[pygame.K_RIGHT]:
+            #         self.dirnx = 1
+            #         self.dirny = 0
+            #         self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+            #         flag = True
+            #
+            #     elif keys[pygame.K_UP]:
+            #         self.dirnx = 0
+            #         self.dirny = -1
+            #         self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+            #         flag = True
+            #
+            #     elif keys[pygame.K_DOWN]:
+            #         self.dirnx = 0
+            #         self.dirny = 1
+            #         self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+            #         flag = True
 
-        elif state == 0:
-            apple_x = apple.location[0]
-            apple_y = apple.location[1]
-
-            p_x = snake.head_position[0]
-            p_y = snake.head_position[1]
-
-            dx = np.absolute(apple_x - p_x)
-            dy = np.absolute(apple_y - p_y)
-
-            # world.reward = 1 - ((dx ** 2 + dy ** 2) ** 0.5 / world.width  )
-            world.reward = 0
-            flag = False
-
-        elif state == 1:    # eaten an apple
-            # print('???????? ', self.snake_positions, self.head_position, prev_head_pos)
-            self.snake_positions[0] = prev_head_pos
-            self.snake_positions.appendleft(self.head_position)
-            # print('?????? after: ', self.snake_positions, self.head_position, prev_head_pos)
-            apple.generate_new_apple()
-            world.score += 1
-            world.reward = 1.0 * self.body_length
-            return False
-
-        for i in range(len(self.snake_positions) - 1, 0, -1):
-            if i == 1:
-                self.snake_positions[i] = self.prev_head_position
-            else:
-                self.snake_positions[i] = self.snake_positions[i-1]
-
-        return flag
-
-    # MOVE ONLY THE HEAD OF THE SNAKE
-    def move_snake(self, action):
         if action == 0:
-            # move snake left
-            self.prev_head_position = list(self.head_position)
-            # print('one: ', self.prev_head_position, self.head_position)
-            snake.head_position[0] -= snake.hitbox
-            # print('two: ', self.prev_head_position, self.head_position)
+            # print("moved left")
+            self.dirnx = -1
+            self.dirny = 0
+            self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+            # flag = True
 
         elif action == 1:
-            # move snake up
-            self.prev_head_position = list(self.head_position)
-            snake.head_position[1] += snake.hitbox
+            # print("moved right")
+            self.dirnx = 1
+            self.dirny = 0
+            self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+            # flag = True
 
         elif action == 2:
-            # move snake right
-            self.prev_head_position = list(self.head_position)
-            snake.head_position[0] += snake.hitbox
+            # print("moved up")
+            self.dirnx = 0
+            self.dirny = -1
+            self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+            # flag = True
 
         elif action == 3:
-            # move snake down
-            self.prev_head_position = list(self.head_position)
-            snake.head_position[1] -= snake.hitbox
+            # print("moved down")
+            self.dirnx = 0
+            self.dirny = 1
+            self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+            # flag = True
 
-        # print(self.prev_head_pos, self.head_position)
-        collision = check_collision(self.head_position)
-        done = self.update_snake(collision, self.prev_head_position)
-        self.body_length = len(self.snake_positions)
+        self.hit_wall = False
 
-        return done
+        for i, c in enumerate(self.body):
+            # print('c: ', c.pos)
+            p = c.pos[:]
+            # if flag:
+            if p in self.turns:
+                # print("test222")
+                turn = self.turns[p]
+                c.move(turn[0], turn[1])
+                if i == len(self.body) - 1:
+                    self.turns.pop(p)
+            else:
+                c.move(c.dirnx, c.dirny)
+                self.hit_wall = False
 
+            if c.dirnx == -1 and c.pos[0] <= 0:
+                # print("hit wall0")
+                # c.pos = (c.rows - 1, c.pos[1])
+                self.hit_wall = True
+            elif c.dirnx == 1 and c.pos[0] >= c.rows - 1:
+                # print("hit wall1")
+                # c.pos = (0, c.pos[1])
+                self.hit_wall = True
+            elif c.dirny == 1 and c.pos[1] >= c.rows - 1:
+                # print("hit wall2")
+                # c.pos = (c.pos[0], 0)
+                self.hit_wall = True
+            elif c.dirny == -1 and c.pos[1] <= 0:
+                # print("hit wall3")
+                # c.pos = (c.pos[0], c.rows - 1)
+                self.hit_wall = True
+            # else:
+            #     c.move(c.dirnx, c.dirny)
+            #     self.hit_wall = False
 
-class Apple:
-    def __init__(self):
-        self.location = [0, 0]
-        self.generate_new_apple()
+    def reset(self, pos):
+        self.done = False
+        self.head = cube(pos)
+        self.body = []
+        self.body.append(self.head)
+        self.turns = {}
+        self.dirnx = 0
+        self.dirny = 1
 
-    def generate_new_apple(self):
-        apple_x = World.width * np.random.random()
-        apple_y = World.height * np.random.random()
-        self.location = [int(apple_x), int(apple_y)]
+    def addCube(self):
+        tail = self.body[-1]
+        dx, dy = tail.dirnx, tail.dirny
 
+        if dx == 1 and dy == 0:
+            self.body.append(cube((tail.pos[0] - 1, tail.pos[1])))
+        elif dx == -1 and dy == 0:
+            self.body.append(cube((tail.pos[0] + 1, tail.pos[1])))
+        elif dx == 0 and dy == 1:
+            self.body.append(cube((tail.pos[0], tail.pos[1] - 1)))
+        elif dx == 0 and dy == -1:
+            self.body.append(cube((tail.pos[0], tail.pos[1] + 1)))
 
-def check_collision(position):
-    apple_x = int(apple.location[0])
-    apple_y = int(apple.location[1])
+        self.body[-1].dirnx = dx
+        self.body[-1].dirny = dy
 
-    p_x = int(position[0])
-    p_y = int(position[1])
-
-    dx = np.absolute(apple_x - p_x)
-    dy = np.absolute(apple_y - p_y)
-
-    # print([apple_x, apple_y], [p_x, p_y])
-    # print(apple.location, position)
-
-    if dx <= snake.hitbox and dy <= snake.hitbox:
-        #   AN APPLE HAS BEEN EATEN
-        # print([apple_x, apple_y], [p_x, p_y])
-        # print(apple.location, position)
-        return 1
-
-    elif 0 > p_x or 0 > p_y or p_x > World.width or p_y > World.height:
-        #   HIT A waLL
-        # print('test2')
-        return -1
-
-    elif position in list(itertools.islice(snake.snake_positions, 1, len(snake.snake_positions))):
-        # print('test3: ', snake.snake_positions)
-        return -1
-
-    elif 0 < p_x < World.width and 0 < p_y < World.height:
-        return 0
-
-
-def start_game():
-    global snake, apple, world, window, fps
-    snake = Snake()
-    apple = Apple()
-    world = World()
-    apple.generate_new_apple()
-    window = pygame.display.set_mode((World.height, World.width))
-    pygame.display.set_caption("snake")
-    fps = pygame.time.Clock()
-
-    return observe_next()
-
-
-def game_over():
-    pygame.quit()
-    sys.exit()
-
-
-def reset():
-    start_game()
+    def draw(self, surface):
+        for i, c in enumerate(self.body):
+            if i == 0:
+                c.draw(surface, True)
+            else:
+                c.draw(surface)
 
 
-def observe_next():
+def drawGrid(w, rows, surface):
+    sizeBtwn = w // rows
 
-    obs = np.zeros([World.state_size, World.state_size])
+    x = 0
+    y = 0
+    for l in range(rows):
+        x = x + sizeBtwn
+        y = y + sizeBtwn
 
-    #   SET CENTER POINT
-    center = int(world.state_size / 2)
-    obs[center: center + 1, center: center + 1] = 1
-
-    apple_loc = apple.location
-    current = snake.head_position
-
-    apple_dx = int(np.absolute(apple_loc[0] - current[0]))
-    apple_dy = int(np.absolute(apple_loc[1] - current[1]))
-
-    if apple_dx <= center and apple_dy <= center:
-
-        # APPLE IS INSIDE OUR AGENT FRAME
-        if apple_loc[0] > current[0] and apple_loc[1] > current[1]:
-            # TOP RIGHT
-            # print(apple_dx, apple_dy)
-            obs[center - apple_dy: center - apple_dy + 1, center + apple_dx: center + apple_dx + 1] = 255.0
-
-        elif apple_loc[0] < current[0] and apple_loc[1] > current[1]:
-            # TOP LEFT
-            obs[center - apple_dy: center - apple_dy + 1, center - apple_dx: center - apple_dx + 1] = 255.0
-
-        elif apple_loc[0] > current[0] and apple_loc[1] < current[1]:
-            # BOT LEFT
-            obs[center + apple_dy: center + apple_dy + 1, center - apple_dx: center - apple_dx + 1] = 255.0
-
-        elif apple_loc[0] < current[0] and apple_loc[1] < current[1]:
-            # BOT RIGHT
-            obs[center + apple_dy: center + apple_dy + 1, center + apple_dx: center + apple_dy + 1] = 255.0
-
-    for pos in snake.snake_positions:
-
-        dx = int(np.absolute(pos[0] - current[0]))
-        dy = int(np.absolute(pos[1] - current[1]))
-
-        if pos[0] > current[0] and pos[1] > current[1]:
-            # TOP RIGHT
-            obs[center - dy: center - dy + 1, center + dx: center + dx + 1] = 1
-
-        elif pos[0] < current[0] and pos[1] > current[1]:
-            # TOP LEFT
-            obs[center - dy: center - dy + 1, center - dx: center - dx + 1] = 1
-
-        elif pos[0] > current[0] and pos[1] < current[1]:
-            # BOT LEFT
-            obs[center + dy: center + dy + 1, center - dx: center - dx + 1] = 1
-
-        elif pos[0] < current[0] and pos[1] < current[1]:
-            # BOT RIGHT
-            obs[center + dy: center + dy + 1, center + dx: center + dx + 1] = 1
-
-    obs = obs.reshape([World.state_size, World.state_size, 1])
-
-    # print(obs.shape)
-
-    return obs
+        pygame.draw.line(surface, (255, 255, 255), (x, 0), (x, w))
+        pygame.draw.line(surface, (255, 255, 255), (0, y), (w, y))
 
 
-def step(action):
+def randomSnack(rows, item):
+    positions = item.body
 
-    color0 = (255, 255, 255)
-    color1 = (0, 255, 0)
-    color2 = (255, 0, 0)
+    while True:
+        x = random.randrange(rows)
+        y = random.randrange(rows)
+        if len(list(filter(lambda z: z.pos == (x, y), positions))) > 0:
+            continue
+        else:
+            break
 
-    window.fill(color0)
+    return (x, y)
 
-    done = snake.move_snake(action)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_over()
+def message_box(subject, content):
+    root = tk.Tk()
+    root.attributes("-topmost", True)
+    root.withdraw()
+    messagebox.showinfo(subject, content)
+    try:
+        root.destroy()
+    except:
+        pass
 
-    pygame.draw.rect(window, color1, pygame.Rect(snake.head_position[0], snake.head_position[1], 10, 10))
 
-    for pos in snake.snake_positions:
-        pygame.draw.rect(window, color1, pygame.Rect(pos[0], pos[1], 10, 10))
+class Game:
+    width = 500
+    prev_dis = 0
+    rows = 20
 
-    pygame.draw.rect(window, color2, pygame.Rect(apple.location[0], apple.location[1], 10, 10))
+    def __init__(self, width=500, rows=20):
+        # global width, rows, s, snack, win
+        self.width = width
+        self.rows = rows
+        self.win = pygame.display.set_mode((width, width))
+        self.s = snake((255, 0, 0), (10, 10))
+        self.snack = cube(randomSnack(self.rows, self.s), color=(0, 255, 0))
+        self.prev_dis = rows
 
-    next_state = observe_next()
-    # print(next_state.shape)
-    pygame.display.update()
-    fps.tick(60)
+    def redrawWindow(self, surface):
+        surface.fill((0, 0, 0))
+        self.s.draw(surface)
+        self.snack.draw(surface)
+        drawGrid(self.width, self.rows, surface)
+        pygame.display.update()
 
-    return next_state, world.reward, world.score, done
+    def reset2(self):
+        # global width, rows, s, snack, win
+        self.width = 500
+        self.rows = 20
+        self.win = pygame.display.set_mode((self.width, self.width))
+        self.s = snake((255, 0, 0), (10, 10))
+        self.snack = cube(randomSnack(self.rows, self.s), color=(0, 255, 0))
 
+        self.s.done = False
+        self.s.head = cube((10, 10))
+        self.s.body = []
+        self.s.body.append(self.s.head)
+        self.s.turns = {}
+        self.s.dirnx = 0
+        self.s.dirny = 1
+
+    def calculate_reward(self, head, apple, prev_d):
+        # print(head, apple)
+
+        snake_x = head[0]
+        snake_y = head[1]
+
+        apple_x = apple[0]
+        apple_y = apple[1]
+
+        distance = ((snake_x - apple_x) ** 2 + (snake_y - apple_y) ** 2) ** 0.5
+
+        self.prev_dis = distance
+
+        if distance == 0: distance = 1
+
+        if distance < prev_d:
+            reward = 1 / distance
+
+        else:
+            reward = 0
+
+        return reward
+
+    def step(self, action, render=False):
+        clock = pygame.time.Clock()
+        self.s.done = False
+        pygame.time.delay(50)
+        clock.tick(30)
+        self.s.move(action)
+
+        reward = 0
+
+        if self.s.body[0].pos == self.snack.pos:
+            self.s.addCube()
+            reward = 1 * len(self.s.body)
+            self.snack = cube(randomSnack(self.rows, self.s), color=(0, 255, 0))
+
+        # x2 = pygame.surfarray.array3d(self.win)
+
+        state = np.zeros((self.rows, self.rows))
+
+        for i in self.s.body:
+            # print(i.pos)
+            x = i.pos[0]
+            y = i.pos[1]
+            state[x][y] = 1
+
+        state[self.snack.pos[0]][self.snack.pos[1]] = 1
+
+        # print(s.head.pos)
+        # reward = self.calculate_reward(self.s.head.pos, self.snack.pos, self.prev_dis)
+
+        for x in range(len(self.s.body)):
+            if self.s.hit_wall or self.s.body[x].pos in list(map(lambda z: z.pos, self.s.body[x + 1:])):
+                print('Score: ', len(self.s.body))
+
+                self.s.done = True
+                reward = -1
+                break
+
+        self.redrawWindow(self.win)
+        # if render:
+        #     win.fill((0, 0, 0))
+        #     s.draw(win)
+        #     snack.draw(win)
+        #     drawGrid(width, rows, win)
+        #     pygame.display.update()
+
+        score = len(self.s.body)
+        return state.reshape((self.rows, self.rows, 1)), reward, score, self.s.done
+
+
+# main()
+#
+# run = True
+#
+# while run:
+#     action = random.randrange(4)
+#     vector, done = step(action)
+#
+#     # print(action)
+#
+#     if done:
+#         s.reset((10, 10))
+#         # run = False
+#
+# print("episode finished")
 
